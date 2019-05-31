c348a010799210b600256b05c2fc71cced453169
Andre Pato <anpato1994@gmail.com>
2019-05-31 12:24:57 -0400

    added seed data for movies and subscriptions

---
+++ netflux/db/migrate/20190531160328_chnage_column_name.rb
@@ -0,0 +1,5 @@
+class ChnageColumnName < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :subscriptions, :type, :tier
+  end
+end
--- netflux/db/schema.rb
+++ netflux/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_31_155424) do
+ActiveRecord::Schema.define(version: 2019_05_31_160328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2019_05_31_155424) do
   end
 
   create_table "subscriptions", force: :cascade do |t|
-    t.string "type"
+    t.string "tier"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
